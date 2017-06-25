12bf5387a2fbe2441c8a6152b3c55950be1e60ec
grkopiec <grkopiec@gmail.com>
2017-06-25 22:09:13 +0200

    Enables serving static resources for product images

--- src/main/java/pl/bookshop/mvc/configuration/WebConfiguration.java
+++ src/main/java/pl/bookshop/mvc/configuration/WebConfiguration.java
@@ -3,9 +3,17 @@ package pl.bookshop.mvc.configuration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import pl.bookshop.utils.StringUtils;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.bookshop.mvc.controllers")
-public class WebConfiguration {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations(StringUtils.resourceLocation);
+	}
 }
---
+++ src/main/java/pl/bookshop/utils/StringUtils.java
@@ -0,0 +1,6 @@
+package pl.bookshop.utils;
+
+public class StringUtils {
+	public static final String resourceLocation = "file:" + System.getProperty("user.home") +
+			System.getProperty("file.separator") + "images" + System.getProperty("file.separator");
+}
