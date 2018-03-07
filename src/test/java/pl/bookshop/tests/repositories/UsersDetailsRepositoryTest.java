753bf926903cd400ba1752f4ef7352ee2d8d56ee
Grzegorz Kopiec <grkopiec@gmail.com>
2018-03-07 21:38:21 +0100

    Do all tests for UsersDetailsRepository

--- src/test/java/pl/bookshop/tests/repositories/UsersDetailsRepositoryTest.java
+++ src/test/java/pl/bookshop/tests/repositories/UsersDetailsRepositoryTest.java
@@ -1,5 +1,6 @@
 package pl.bookshop.tests.repositories;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,6 +39,7 @@ public class UsersDetailsRepositoryTest {
 	@UsingDataSet(locations = {"/testSchemas/mongo.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
 	public void test_findByUserId_success() {
 		Optional<UserDetails> userDetails = usersDetailsRepository.findByUserId(0L);
+		
 		Assert.assertTrue(userDetails.isPresent());
 	}
 	
@@ -45,6 +47,54 @@ public class UsersDetailsRepositoryTest {
 	@UsingDataSet(locations = {"/testSchemas/mongo.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
 	public void test_findByUserId_notExistFail() {
 		Optional<UserDetails> userDetails = usersDetailsRepository.findByUserId(2L);
+		
 		Assert.assertFalse(userDetails.isPresent());
+	}
+	
+	@Test
+	@UsingDataSet(locations = {"/testSchemas/mongo.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+	public void test_findByUserIdIn_success() {
+		List<Long> usersIds = new ArrayList<>();
+		usersIds.add(0L);
+		usersIds.add(1L);
+		List<UserDetails> usersDetails = usersDetailsRepository.findByUserIdIn(usersIds);
+		
+		Assert.assertEquals(2, usersDetails.size());
+	}
+	
+	/**
+	 * In this scenario from <code>Mongo</code> database are finding two ids, one id exists but second does not exist.
+	 */
+	@Test
+	@UsingDataSet(locations = {"/testSchemas/mongo.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+	public void test_findByUserIdIn_partlyNotFound() {
+		List<Long> usersIds = new ArrayList<>();
+		usersIds.add(1L);
+		usersIds.add(2L);
+		List<UserDetails> usersDetails = usersDetailsRepository.findByUserIdIn(usersIds);
+		
+		Assert.assertEquals(1, usersDetails.size());
+	}
+	
+	@Test
+	@UsingDataSet(locations = {"/testSchemas/mongo.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+	public void test_deleteByUserId_success() {
+		List<UserDetails> allUsersDetailsBefore = usersDetailsRepository.findAll();
+		usersDetailsRepository.deleteByUserId(0L);
+		List<UserDetails> allUsersDetailsAfter = usersDetailsRepository.findAll();
+		
+		Assert.assertEquals(2, allUsersDetailsBefore.size());
+		Assert.assertEquals(1, allUsersDetailsAfter.size());
+	}
+	
+	@Test
+	@UsingDataSet(locations = {"/testSchemas/mongo.json"}, loadStrategy = LoadStrategyEnum.CLEAN_INSERT)
+	public void test_deleteByUserId_deleteNotExists() {
+		List<UserDetails> allUsersDetailsBefore = usersDetailsRepository.findAll();
+		usersDetailsRepository.deleteByUserId(2L);
+		List<UserDetails> allUsersDetailsAfter = usersDetailsRepository.findAll();
+		
+		Assert.assertEquals(2, allUsersDetailsBefore.size());
+		Assert.assertEquals(2, allUsersDetailsAfter.size());
 	}
 }
