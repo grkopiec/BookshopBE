691dd5bb2d7cbe9829801eb8ffcbee0d6ca7d82e
Grzegorz Kopiec <grkopiec@gmail.com>
2018-03-14 21:56:13 +0100

    Add new fields to UserDetails object

--- src/main/java/pl/bookshop/domains/mongo/UserDetails.java
+++ src/main/java/pl/bookshop/domains/mongo/UserDetails.java
@@ -12,6 +12,12 @@ public class UserDetails {
 	private Long userId;
 	private String name;
 	private String surname;
+	private String email;
+	private String phone;
+	private String city;
+	private String street;
+	private String state;
+	private String zipCode;
 
 	public String getId() {
 		return id;
@@ -44,15 +50,64 @@ public class UserDetails {
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
 
 	@Override
 	public String toString() {
-		return "UserDetail [id=" + id + ", userId=" + userId + ", name=" + name + ", surname=" + surname + "]";
+		return "UserDetails [id=" + id + ", userId=" + userId + ", name=" + name + ", surname=" + surname + ", email=" + email + ", phone=" + phone
+				+ ", city=" + city + ", street=" + street + ", state=" + state + ", zipCode=" + zipCode + "]";
 	}
-	
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, userId, name, surname);
+		return Objects.hash(id, userId, name, surname, email, phone, city, street, state, zipCode);
 	}
 	
 	@Override
@@ -68,7 +123,9 @@ public class UserDetails {
 		}
 		
 		UserDetails other = (UserDetails) obj;
-		return Objects.equals(this.id, other.id) && Objects.equals(this.userId, other.userId)
-				&& Objects.equals(this.name, other.name) && Objects.equals(this.surname, other.surname);
+		return Objects.equals(this.id, other.id) && Objects.equals(this.userId, other.userId) && Objects.equals(this.name, other.name)
+				&& Objects.equals(this.surname, other.surname) && Objects.equals(this.email, other.email) && Objects.equals(this.phone, other.phone)
+				&& Objects.equals(this.city, other.city) && Objects.equals(this.street, other.street) && Objects.equals(this.state, other.state)
+				&& Objects.equals(this.zipCode, other.zipCode);
 	}
 }
