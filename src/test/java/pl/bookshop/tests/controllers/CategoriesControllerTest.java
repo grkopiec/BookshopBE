9656597fac55b049e05bbc8dcc194b8120e99dc2
Grzegorz Kopiec <grkopiec@gmail.com>
2018-03-22 21:24:18 +0100

    Fix tests after new changes in categoriesController

--- src/test/java/pl/bookshop/tests/controllers/CategoriesControllerTest.java
+++ src/test/java/pl/bookshop/tests/controllers/CategoriesControllerTest.java
@@ -41,7 +41,7 @@ public class CategoriesControllerTest {
 	
 	@Test
 	public void test_findAll_success() throws Exception {
-		List<Category> categories = Arrays.asList(getCategory1(), getCategory2());
+		List<Category> categories = Arrays.asList(getCategory0(), getCategory1());
 		
 		Mockito.when(categoriesService.findAll()).thenReturn(categories);
 		
@@ -60,7 +60,7 @@ public class CategoriesControllerTest {
 	
 	@Test
 	public void test_findOne_success() throws Exception {
-		Category category1 = getCategory1();
+		Category category1 = getCategory0();
 		
 		Mockito.when(categoriesService.findOne(category1.getId())).thenReturn(category1);
 		
@@ -79,7 +79,7 @@ public class CategoriesControllerTest {
 	 */
 	@Test
 	public void test_findOne_fail() throws Exception {
-		Category category1 = getCategory1();
+		Category category1 = getCategory0();
 		
 		Mockito.when(categoriesService.findOne(1L)).thenReturn(null);
 		
@@ -92,7 +92,7 @@ public class CategoriesControllerTest {
 	
 	@Test
 	public void test_create_success() throws Exception {
-		Category category1 = getCategory1();
+		Category category1 = getCategory0();
 		
 		Mockito.when(categoriesService.isExist(category1)).thenReturn(false);
 		Mockito.doNothing().when(categoriesService).create(category1);
@@ -114,7 +114,7 @@ public class CategoriesControllerTest {
 	 */
 	@Test
 	public void test_create_fail() throws Exception {
-		Category category1 = getCategory1();
+		Category category1 = getCategory0();
 		
 		Mockito.when(categoriesService.isExist(category1)).thenReturn(true);
 		
@@ -130,80 +130,110 @@ public class CategoriesControllerTest {
 	
 	/**
 	 * Scenario for this test is like this, first we are sending to PUT endpoint id exist object in database that will be replaced
-	 * and new object, next in service we are finding object in database to replace. In object we passed to endpoint we are changing
-	 * id on this that already contains exists object in database, and after it we replace object into new, but we left the same id
+	 * and new object, next in service we are finding object in database to replace. Passed object and object which will be replaced
+	 * have different names. In object we passed to endpoint we are changing id on this that already contains exists object in
+	 * database, and after it we replace object into new, but we left the same id
 	 */
 	@Test
-	public void test_update_success() throws Exception {
-		Category previousCategory = getCategory1();
-		Category beforeUpdateCategory = getCategory2();
-		Category afterUpdateCategory = getCategory2();
+	public void test_update_successWithDifferentNames() throws Exception {
+		Category category = getCategory0();
+		Category updatingCategory = getCategory1();
+		Category updatedCategory = getCategory2();
 
-		Mockito.when(categoriesService.isExist(beforeUpdateCategory)).thenReturn(false);
-		Mockito.when(categoriesService.update(previousCategory.getId(), beforeUpdateCategory)).thenReturn(afterUpdateCategory);
+		Mockito.when(categoriesService.findOne(category.getId())).thenReturn(updatingCategory);
+		Mockito.when(categoriesService.isExist(category)).thenReturn(false);
+		Mockito.when(categoriesService.update(category.getId(), category)).thenReturn(updatedCategory);
 		
 		mockMvc.perform(MockMvcRequestBuilders
-						.put("/categories/{id}", previousCategory.getId())
+						.put("/categories/{id}", category.getId())
 						.contentType(MediaType.APPLICATION_JSON)
-						.content(TestUtils.toJson(beforeUpdateCategory)))
+						.content(TestUtils.toJson(category)))
 				.andExpect(MockMvcResultMatchers.status().isOk())
-				.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(afterUpdateCategory.getId().intValue())))
-				.andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(afterUpdateCategory.getName())));
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(updatedCategory.getId().intValue())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(updatedCategory.getName())));
 		
-		Mockito.verify(categoriesService, Mockito.times(1)).isExist(beforeUpdateCategory);
-		Mockito.verify(categoriesService, Mockito.times(1)).update(previousCategory.getId(), beforeUpdateCategory);
+		Mockito.verify(categoriesService, Mockito.times(1)).findOne(category.getId());
+		Mockito.verify(categoriesService, Mockito.times(1)).isExist(category);
+		Mockito.verify(categoriesService, Mockito.times(1)).update(category.getId(), category);
 		Mockito.verifyNoMoreInteractions(categoriesService);
 	}
 	
 	/**
-	 * Should occur 404 code error, do not found category
-	 * In this scenario object will not be change because do not exist requested id
+	 * Scenario for this test is like this, first we are sending to PUT endpoint id exist object in database that will be replaced
+	 * and new object, next in service we are finding object in database to replace. Passed object and object which will be replaced
+	 * have the same names. In object we passed to endpoint we are changing id on this that already contains exists object in
+	 * database, and after it we replace object into new, but we left the same id
+	 */
+	@Test
+	public void test_update_successWithSameNames() throws Exception {
+		Category category = getCategory0();
+		Category updatingCategory = getCategory1();
+		updatingCategory.setName(category.getName());
+		Category updatedCategory = getCategory2();
+
+		Mockito.when(categoriesService.findOne(category.getId())).thenReturn(updatingCategory);
+		Mockito.when(categoriesService.update(category.getId(), category)).thenReturn(updatedCategory);
+		
+		mockMvc.perform(MockMvcRequestBuilders
+						.put("/categories/{id}", category.getId())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(TestUtils.toJson(category)))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(updatedCategory.getId().intValue())))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(updatedCategory.getName())));
+		
+		Mockito.verify(categoriesService, Mockito.times(1)).findOne(category.getId());
+		Mockito.verify(categoriesService, Mockito.times(1)).update(category.getId(), category);
+		Mockito.verifyNoMoreInteractions(categoriesService);
+	}
+	
+	/**
+	 * Should occur 404 code error, do not found category. In this scenario object will not be change because do not exist requested id
 	 */
 	@Test
 	public void test_update_doNotFindIdfail() throws Exception {
-		Category previousCategory = getCategory1();
-		Category beforeUpdateCategory = getCategory2();
+		Category category = getCategory0();
 
-		Mockito.when(categoriesService.isExist(beforeUpdateCategory)).thenReturn(false);
-		Mockito.when(categoriesService.update(previousCategory.getId(), beforeUpdateCategory)).thenReturn(null);
+		Mockito.when(categoriesService.findOne(category.getId())).thenReturn(null);
 		
 		mockMvc.perform(MockMvcRequestBuilders
-						.put("/categories/{id}", previousCategory.getId())
+						.put("/categories/{id}", category.getId())
 						.contentType(MediaType.APPLICATION_JSON)
-						.content(TestUtils.toJson(beforeUpdateCategory)))
+						.content(TestUtils.toJson(category)))
 				.andExpect(MockMvcResultMatchers.status().isNotFound());
 		
-		Mockito.verify(categoriesService, Mockito.times(1)).isExist(beforeUpdateCategory);
-		Mockito.verify(categoriesService, Mockito.times(1)).update(previousCategory.getId(), beforeUpdateCategory);
+		Mockito.verify(categoriesService, Mockito.times(1)).findOne(category.getId());
 		Mockito.verifyNoMoreInteractions(categoriesService);
 	}
 	
 	
 	/**
-	 * Should occur 409 code error, category name already exists
-	 * In this scenario object will not be change because category name already exists
+	 * Should occur 409 code error, category name already exists. In this scenario object will not be change because category name
+	 * already exists
 	 */
 	@Test
 	public void test_update_nameColisionFail() throws Exception {
-		Category previousCategory = getCategory1();
-		Category beforeUpdateCategory = getCategory2();
+		Category category = getCategory0();
+		Category updatingCategory = getCategory1();
 
 		
-		Mockito.when(categoriesService.isExist(beforeUpdateCategory)).thenReturn(true);
+		Mockito.when(categoriesService.findOne(category.getId())).thenReturn(updatingCategory);
+		Mockito.when(categoriesService.isExist(category)).thenReturn(true);
 		
 		mockMvc.perform(MockMvcRequestBuilders
-						.put("/categories/{id}", previousCategory.getId())
+						.put("/categories/{id}", category.getId())
 						.contentType(MediaType.APPLICATION_JSON)
-						.content(TestUtils.toJson(beforeUpdateCategory)))
+						.content(TestUtils.toJson(category)))
 				.andExpect(MockMvcResultMatchers.status().isConflict());
 		
-		Mockito.verify(categoriesService, Mockito.times(1)).isExist(beforeUpdateCategory);
+		Mockito.verify(categoriesService, Mockito.times(1)).findOne(category.getId());
+		Mockito.verify(categoriesService, Mockito.times(1)).isExist(category);
 		Mockito.verifyNoMoreInteractions(categoriesService);
 	}
 	
 	@Test
 	public void test_delete_success() throws Exception {
-		Category category1 = getCategory1();
+		Category category1 = getCategory0();
 		
 		Mockito.when(categoriesService.findOne(category1.getId())).thenReturn(category1);
 		Mockito.doNothing().when(categoriesService).delete(category1.getId());
@@ -222,7 +252,7 @@ public class CategoriesControllerTest {
 	 */
 	@Test
 	public void test_delete_fail() throws Exception {
-		Category category1 = getCategory1();
+		Category category1 = getCategory0();
 		
 		Mockito.when(categoriesService.findOne(category1.getId())).thenReturn(null);
 		
@@ -234,10 +264,17 @@ public class CategoriesControllerTest {
 		Mockito.verifyNoMoreInteractions(categoriesService);
 	}
 	
+	private Category getCategory0() {
+		Category category0 = new Category();
+		category0.setId(1L);
+		category0.setName(RandomStringUtils.randomAlphabetic(10));
+		return category0;
+	}
+	
 	private Category getCategory1() {
 		Category category1 = new Category();
-		category1.setId(1L);
-		category1.setName(RandomStringUtils.randomAlphabetic(10));
+		category1.setId(2L);
+		category1.setName(RandomStringUtils.randomAlphabetic(5));
 		return category1;
 	}
 	
