9051c4edec3bb78d447b436e02a09961037e7b6f
Grzegorz Kopiec <grkopiec@gmail.com>
2018-03-30 23:15:41 +0200

    Add exception handler for next validation exception

--- src/main/java/pl/bookshop/mvc/controllers/ExceptionHandlerController.java
+++ src/main/java/pl/bookshop/mvc/controllers/ExceptionHandlerController.java
@@ -2,6 +2,10 @@ package pl.bookshop.mvc.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
+	@ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException exception) {
+		List<String> validationErrors = new ArrayList<>();
+		
+		Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
+    	for (ConstraintViolation<?> constraintViolation: constraintViolations) {
+    		String error = constraintViolation.getMessage();
+    		validationErrors.add(error);
+    	}
+    	return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
 		List<String> validationErrors = new ArrayList<>();
@@ -24,3 +40,4 @@ public class ExceptionHandlerController {
     	return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
 }
+
