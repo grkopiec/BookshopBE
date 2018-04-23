b42eb79b920e6f31db2a47842770367babe9ba79
Grzegorz Kopiec <grkopiec@gmail.com>
2018-04-23 22:14:14 +0200

    Add tests for UserDetails email

--- src/test/java/pl/bookshop/tests/validation/UserDetailsValidationTest.java
+++ src/test/java/pl/bookshop/tests/validation/UserDetailsValidationTest.java
@@ -1,6 +1,8 @@
 package pl.bookshop.tests.validation;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -15,6 +17,7 @@ import org.junit.Test;
 
 import pl.bookshop.domains.mongo.UserDetails;
 import pl.bookshop.mvc.validation.AdminUser;
+import pl.bookshop.mvc.validation.NormalUser;
 
 public class UserDetailsValidationTest {
 	private static Validator validator;
@@ -108,10 +111,209 @@ public class UserDetailsValidationTest {
 		Assert.assertEquals("User surname: " + surname + " size must be between 2 and 50", iterator.next().getMessage());
 	}
 	
+	public void test_normalUserDetailsAllFields_success() {
+		UserDetails userDetails = getValidNormalUserDetails();
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(0, constraintViolations.size());
+	}
+	
+	@Test
+	public void test_normalUserDetailsName_whenIsNull() {
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setName(null);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User name cannot be empty", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsName_whenIsTooShort() {
+		String name = RandomStringUtils.randomAlphabetic(1);
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setName(name);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User name: " + name + " size must be between 2 and 50", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsName_whenIsTooLong() {
+		String name = RandomStringUtils.randomAlphabetic(51);
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setName(name);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User name: " + name + " size must be between 2 and 50", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsSurname_whenIsNull() {
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setSurname(null);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User surname cannot be empty", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsSurname_whenIsTooShort() {
+		String surname = RandomStringUtils.randomAlphabetic(1);
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setSurname(surname);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User surname: " + surname + " size must be between 2 and 50", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsSurname_whenIsTooLong() {
+		String surname = RandomStringUtils.randomAlphabetic(51);
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setSurname(surname);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User surname: " + surname + " size must be between 2 and 50", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsEmail_whenIsNull() {
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setEmail(null);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User email cannot be empty", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsEmail_whenIsTooShort() {
+		String email = RandomStringUtils.randomAlphabetic(1).toLowerCase() + '@' + RandomStringUtils.randomAlphabetic(1).toLowerCase()
+				+ '.' + RandomStringUtils.randomAlphabetic(1).toLowerCase();
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setEmail(email);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(2, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		
+		List<String> occurErrors = new ArrayList<>();
+		occurErrors.add(iterator.next().getMessage());
+		occurErrors.add(iterator.next().getMessage());
+		
+		List<String> expectedErrors = new ArrayList<>();
+		expectedErrors.add("User email is not valid");
+		expectedErrors.add("User email: " + email + " size must be between 6 and 70");
+		
+		Assert.assertTrue(occurErrors.contains(expectedErrors.get(0)));
+		Assert.assertTrue(occurErrors.contains(expectedErrors.get(1)));
+	}
+	
+	@Test
+	public void test_normalUserDetailsEmail_whenIsTooLong() {
+		String email = RandomStringUtils.randomAlphabetic(50).toLowerCase() + '@' + RandomStringUtils.randomAlphabetic(10).toLowerCase()
+				+ '.' + RandomStringUtils.randomAlphabetic(10).toLowerCase();
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setEmail(email);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(2, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		
+		List<String> occurErrors = new ArrayList<>();
+		occurErrors.add(iterator.next().getMessage());
+		occurErrors.add(iterator.next().getMessage());
+		
+		List<String> expectedErrors = new ArrayList<>();
+		expectedErrors.add("User email is not valid");
+		expectedErrors.add("User email: " + email + " size must be between 6 and 70");
+		
+		Assert.assertTrue(occurErrors.contains(expectedErrors.get(0)));
+		Assert.assertTrue(occurErrors.contains(expectedErrors.get(1)));
+	}
+	
+	@Test
+	public void test_normalUserDetailsEmail_whenIsNotTypical() {
+		String email = RandomStringUtils.randomAlphabetic(5).toLowerCase() + '.' + RandomStringUtils.randomNumeric(5) + '.'
+				+ RandomStringUtils.randomAlphabetic(5).toLowerCase() + '_' + RandomStringUtils.randomAlphabetic(5).toLowerCase() + '@'
+				+ RandomStringUtils.randomAlphabetic(10).toLowerCase() + '.' + RandomStringUtils.randomAlphabetic(10).toLowerCase() + '.'
+				+ RandomStringUtils.randomNumeric(5) + '.' + RandomStringUtils.randomAlphabetic(5).toLowerCase();
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setEmail(email);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(0, constraintViolations.size());
+	}
+	
+	@Test
+	public void test_normalUserDetailsEmail_whenNumbersAtEnd() {
+		String email = RandomStringUtils.randomAlphabetic(50).toLowerCase() + '@' + RandomStringUtils.randomAlphabetic(5).toLowerCase() + '.'
+				+ RandomStringUtils.randomAlphabetic(5).toLowerCase() + '.' + RandomStringUtils.randomNumeric(5);
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setEmail(email);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User email is not valid", iterator.next().getMessage());
+	}
+	
+	@Test
+	public void test_normalUserDetailsEmail_whenHyfenInFrontPart() {
+		String email = RandomStringUtils.randomAlphabetic(20).toLowerCase() + '-' + RandomStringUtils.randomAlphabetic(20).toLowerCase() + '@'
+				+ RandomStringUtils.randomAlphabetic(5).toLowerCase() + '.' + RandomStringUtils.randomAlphabetic(5).toLowerCase() + '.'
+				+ RandomStringUtils.randomNumeric(5);
+		UserDetails userDetails = getValidNormalUserDetails();
+		userDetails.setEmail(email);
+		
+		Set<ConstraintViolation<UserDetails>> constraintViolations = validator.validate(userDetails, NormalUser.class);
+		Assert.assertEquals(1, constraintViolations.size());
+		
+		Iterator<ConstraintViolation<UserDetails>> iterator = constraintViolations.iterator();
+		Assert.assertEquals("User email is not valid", iterator.next().getMessage());
+	}
+	
 	private UserDetails getValidAdminUserDetails() {
 		UserDetails userDetails = new UserDetails();
 		userDetails.setName(RandomStringUtils.randomAlphabetic(30));
 		userDetails.setSurname(RandomStringUtils.randomAlphabetic(50));
+		return userDetails;
+	}
+	
+	private UserDetails getValidNormalUserDetails() {
+		UserDetails userDetails = new UserDetails();
+		userDetails.setName(RandomStringUtils.randomAlphabetic(30));
+		userDetails.setSurname(RandomStringUtils.randomAlphabetic(50));
+		userDetails.setEmail(RandomStringUtils.randomAlphabetic(20).toLowerCase() + '@'
+				+ RandomStringUtils.randomAlphabetic(10).toLowerCase() + '.' + RandomStringUtils.randomAlphabetic(5).toLowerCase());
+		userDetails.setPhone(RandomStringUtils.randomNumeric(9));
+		userDetails.setCity(RandomStringUtils.randomAlphabetic(20));
+		userDetails.setStreet(RandomStringUtils.randomAlphabetic(40));
+		userDetails.setState(RandomStringUtils.randomAlphabetic(20));
+		userDetails.setZipCode(RandomStringUtils.randomAlphabetic(5));
 		return userDetails;
 	}
 }
