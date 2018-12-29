bd0923a5a8fd74fbecf45ff978c4298868aab939
Grzegorz Kopiec <grkopiec@gmail.com>
2018-12-29 18:41:19 +0100

    Add separated exception handler for SQL exception

--- src/main/java/pl/bookshop/mvc/controllers/ExceptionHandlerController.java
+++ src/main/java/pl/bookshop/mvc/controllers/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package pl.bookshop.mvc.controllers;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -7,6 +8,7 @@ import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
@@ -19,7 +21,7 @@ public class ExceptionHandlerController {
 	@ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<List<String>> handleConstraintViolationException(ConstraintViolationException exception) {
 		List<String> validationErrors = new ArrayList<>();
-		
+
 		Set<ConstraintViolation<?>> constraintViolations = exception.getConstraintViolations();
     	for (ConstraintViolation<?> constraintViolation: constraintViolations) {
     		String error = constraintViolation.getMessage();
@@ -27,11 +29,11 @@ public class ExceptionHandlerController {
     	}
     	return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
-	
+
 	@ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
 		List<String> validationErrors = new ArrayList<>();
-		
+
 		List<ObjectError> errors = exception.getBindingResult().getAllErrors();
     	for (ObjectError error: errors) {
     		String validationError = error.getDefaultMessage();
@@ -39,4 +41,17 @@ public class ExceptionHandlerController {
     	}
     	return new ResponseEntity<>(validationErrors, HttpStatus.BAD_REQUEST);
     }
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleUnexpectedException(Exception exception) {
+		Throwable rootCause = ExceptionUtils.getRootCause(exception);
+		if (rootCause instanceof SQLException) {
+			return handleSQLException(rootCause);
+		}
+		return new ResponseEntity<>("Occur unexpected exception: " + exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	public ResponseEntity<String> handleSQLException(Throwable sqlException) {
+		return new ResponseEntity<>("Occur SQL exception: " + sqlException.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
