fae22f50f3d482b92720cfd6c2c4a2d9d6d7073a
Grzegorz Kopiec <grkopiec@gmail.com>
2019-05-04 02:22:26 +0200

    Add CORS allowing for development profile

--- src/main/java/pl/bookshop/configuration/security/SpringSecurityConfiguration.java
+++ src/main/java/pl/bookshop/configuration/security/SpringSecurityConfiguration.java
@@ -1,8 +1,12 @@
 package pl.bookshop.configuration.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -14,6 +18,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import pl.bookshop.configuration.filters.AuthenticationTokenFilter;
 
@@ -22,6 +29,9 @@ import pl.bookshop.configuration.filters.AuthenticationTokenFilter;
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService userDetailsService;
+	
+	@Value("${cors.enable}")
+	private boolean corsEanble;
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -45,6 +55,19 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
         authenticationTokenFilter.setAuthenticationManager(authenticationManagerBean());
         return authenticationTokenFilter;
     }
+	
+    @Profile("development")
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -58,6 +81,9 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
 				.and().addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class)
 				.csrf().disable();
+		if (corsEanble) {
+			http.cors();
+		}
 	}
 
 	@Override
--- src/main/java/pl/bookshop/mvc/configuration/WebConfiguration.java
+++ src/main/java/pl/bookshop/mvc/configuration/WebConfiguration.java
@@ -1,8 +1,10 @@
 package pl.bookshop.mvc.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,8 +16,18 @@ import pl.bookshop.utils.Constants;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @ComponentScan(basePackages = "pl.bookshop.mvc.controllers")
 public class WebConfiguration implements WebMvcConfigurer {
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations(Constants.resourceLocation);
 	}
+	
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+		if (activeProfile.equals("development")) {
+    		registry.addMapping("/**");
+    	}
+    }
 }
